1495caef2f0b2f4b2f74a6d9f01b7e3b7843c84e
Sean Wei <seanwei@Seans-MBP.fios-router.home>
2020-09-29 15:06:43 -0400

    1a and b

---
+++ homeworks/ps5B/scrape_justices.R
@@ -0,0 +1,8 @@
+url <- "https://en.wikipedia.org/wiki/List_of_justices_of_the_Supreme_Court_of_the_United_States"
+justices <- url %>% 
+  read_html() %>% 
+  html_nodes("table")
+justices <- html_table(justices[[2]], fill = TRUE)
+
+path <- "/Users/seanwei/Desktop/STAT231-swei1999/homeworks"
+write_csv(x = justices, path = paste0(path, "/justices.csv"))
